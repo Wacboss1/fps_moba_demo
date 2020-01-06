cefff67d3294a20022bef97b42801d4fd02d4a52
William Clemmons Jr <wacboss@live.com>
2020-01-05 22:26:00 -0600

    Player now moves and looks around

---
+++ Assets/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float cameraSpeed = 5;
+
+    float rotate = 0f;
+
+    Transform trans;
+    Transform playerTrans;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Look();
+
+    }
+
+    private void Look()
+    {
+        float xMouse = Input.GetAxis("Mouse X") * cameraSpeed;  //mouse movement left and right
+        float yMouse = Input.GetAxis("Mouse Y") * cameraSpeed;  //mouse movement up and down
+
+        rotate -= yMouse;
+        rotate = Mathf.Clamp(rotate, -90f, 90f);
+
+        trans.localRotation = Quaternion.Euler(rotate, 0f, 0f);
+
+        playerTrans.Rotate(Vector3.up, xMouse);
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 5;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * movementSpeed);
+        rb.AddRelativeForce(Vector3.right * Input.GetAxis("Horizontal") * movementSpeed);
+    }
+}
