ae858897aceafcc124288ac22db315990ef52f33
William Clemmons Jr <wacboss@live.com>
2020-01-06 18:53:49 -0600

    Reworked player movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,18 +5,47 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 5;
+    [SerializeField] float gravity = 9.81f;
+    [SerializeField] float groundDistance = 1f;
 
-    Rigidbody rb;
+
+    public LayerMask groundmask;
+
+    bool isGrounded;
+    Vector3 velocity;
+    GameObject groundChecker;
+    Transform groundCheck;
+    CharacterController charC;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        charC = GetComponent<CharacterController>();
+        groundChecker = GameObject.Find("GroundChecker");
+        groundCheck = groundChecker.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * movementSpeed);
-        rb.AddRelativeForce(Vector3.right * Input.GetAxis("Horizontal") * movementSpeed);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundmask);
+        Move();
+
+        velocity.y -= gravity * Time.deltaTime;
+        charC.Move(velocity * Time.deltaTime);
+
+        if (isGrounded && velocity.y < 0f)
+        {
+            velocity.y = -2f;
+        }
+    }
+
+    private void Move()
+    {
+        float xin = Input.GetAxis("Horizontal");
+        float zin = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * xin + transform.forward * zin;
+
+        charC.Move(move * movementSpeed * Time.deltaTime);
     }
 }
